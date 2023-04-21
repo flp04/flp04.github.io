c836de8081520141209f28e7c39fea252a43b703
Filipe Husak <69702727+flp04@users.noreply.github.com>
2023-04-21 18:57:45 -0300

    correção

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-  header('Cache-Control: no-cache, must-revalidate')
-  header('Content-Type: application/json; charset=utf-8')
+
+// Permitir solicitações de qualquer origem (*)
+header('Access-Control-Allow-Origin: *');
+// Permitir solicitações com os métodos GET e POST
+header('Access-Control-Allow-Methods: GET, POST');
+// Permitir os cabeçalhos "Content-Type" e "Authorization"
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Arquivo PHP (arquivo.php)
+
+// Dados em PHP
+$dados = array(
+  'nome' => 'João',
+  'idade' => 30,
+  'cidade' => 'São Paulo'
+);
+
+// Definir o cabeçalho de resposta como JSON
+header('Content-Type: application/json');
+
+// Converter os dados em PHP para JSON e enviar como resposta
+echo json_encode($dados);
+?>
