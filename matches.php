b4c0733d2a9c8be3973789dd8ff2a7fc04c0416a
Filipe Husak <69702727+flp04@users.noreply.github.com>
2023-04-23 21:20:24 -0300

    atualizado

---
+++ matches.php
@@ -0,0 +1,38 @@
+<?php
+
+// Permitir solicitações de qualquer origem (*)
+header('Access-Control-Allow-Origin: *');
+// Permitir solicitações com os métodos GET e POST
+header('Access-Control-Allow-Methods: GET, POST');
+// Permitir os cabeçalhos "Content-Type" e "Authorization"
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Arquivo PHP (arquivo.php)
+
+// Dados em PHP
+$dados = array(
+  'nome' => 'João',
+  'idade' => 30,
+  'cidade' => 'São Paulo'
+);
+
+// Definir o cabeçalho de resposta como JSON
+header('Content-Type: application/json');
+
+$uri = 'http://api.football-data.org/v4/competitions/';
+  $reqPrefs['http']['method'] = 'GET';
+  $reqPrefs['http']['header'] = 'X-Auth-Token: bef4843a927541c5b894ff0c1d3c78ae';
+  $stream_context = stream_context_create($reqPrefs);
+  $response = file_get_contents($uri, false, $stream_context);
+
+// echo $competitions->competitions[0]->code;
+$uri = 'http://api.football-data.org/v4/competitions/BSA/matches';
+  $reqPrefs['http']['method'] = 'GET';
+  $reqPrefs['http']['header'] = 'X-Auth-Token: bef4843a927541c5b894ff0c1d3c78ae';
+  $stream_context = stream_context_create($reqPrefs);
+  $response = file_get_contents($uri, false, $stream_context);
+  $matches = json_decode($response);
+//   $competitions[] = json_decode($response);
+echo print_r($matches);
+
+?>
