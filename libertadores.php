d1704debcea5ff54bd25879e70d51fda6c1aaa73
Filipe Husak <69702727+flp04@users.noreply.github.com>
2023-05-01 19:17:04 -0300

    corrigido header

--- libertadores.php
+++ libertadores.php
@@ -5,7 +5,8 @@
     // Permitir solicitações com os métodos GET e POST
     header('Access-Control-Allow-Methods: GET, POST');
     // Permitir os cabeçalhos "Content-Type" e "Authorization"
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    // header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    header('Access-Control-Allow-Headers: Content-Type: text/plain, Authorization');
 
     // Arquivo PHP (arquivo.php)
 
