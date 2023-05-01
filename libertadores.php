78f7f6032b9989103d0df1c3f4e1fd31e2b57ce9
Filipe Husak <69702727+flp04@users.noreply.github.com>
2023-05-01 20:38:15 -0300

    corrigido libertadores com arquivo json para gitpages

--- libertadores.php
+++ libertadores.php
@@ -17,5 +17,27 @@
     $stream_context = stream_context_create($reqPrefs);
     $response = file_get_contents($uri, false, $stream_context);
     $standings = json_encode($response);
-    echo $standings;
+    echo 'classificação ok';
+
+    // Converter os dados em PHP para JSON e enviar como resposta
+    $nome_arquivo = 'libertadores.json';
+    $handle = fopen($nome_arquivo, 'w');
+    fwrite($handle, $standings);
+    fclose($handle);
+    echo 'arquivo salvo <br>';
+
+    $uri = 'http://api.football-data.org/v4/competitions/CLI/matches';
+    $reqPrefs['http']['method'] = 'GET';
+    $reqPrefs['http']['header'] = 'X-Auth-Token: bef4843a927541c5b894ff0c1d3c78ae';
+    $stream_context = stream_context_create($reqPrefs);
+    $response = file_get_contents($uri, false, $stream_context);
+    $matches = json_encode($response);
+    echo 'partidas ok';
+
+    // Converter os dados em PHP para JSON e enviar como resposta
+    $nome_arquivo = 'libertadores_partidas.json';
+    $handle = fopen($nome_arquivo, 'w');
+    fwrite($handle, $matches);
+    fclose($handle);
+    echo 'arquivo salvo <br>';
 ?>
