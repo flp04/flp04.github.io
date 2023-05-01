3b895a714075d6fe4df05ee9370e0554264ce53b
Filipe Husak <69702727+flp04@users.noreply.github.com>
2023-05-01 19:42:49 -0300

    atualizado

--- libertadores.php
+++ libertadores.php
@@ -1,5 +1,4 @@
 <?php
-
     // Permitir solicitações de qualquer origem (*)
     header('Access-Control-Allow-Origin: *');
     // Permitir solicitações com os métodos GET e POST
@@ -9,21 +8,8 @@
 
     // header('Content-Type: text/plain');
 
-    ob_start();
-
-    // Arquivo PHP (arquivo.php)
-
-    // Dados em PHP
-    $dados = array(
-    'nome' => 'João',
-    'idade' => 30,
-    'cidade' => 'São Paulo'
-    );
-
     // Definir o cabeçalho de resposta como JSON
     header('Content-Type: application/json');
-
-    // $requisicao = $_POST['requisicao'];
 
     $uri = 'http://api.football-data.org/v4/competitions/CLI/standings';
     $reqPrefs['http']['method'] = 'GET';
@@ -32,47 +18,4 @@
     $response = file_get_contents($uri, false, $stream_context);
     $standings = json_encode($response);
     echo $standings;
-
-    // $uri = 'http://api.football-data.org/v4/competitions/CLI/standings';
-    // $reqPrefs['http']['method'] = 'GET';
-    // $reqPrefs['http']['header'] = 'X-Auth-Token: bef4843a927541c5b894ff0c1d3c78ae';
-    // $stream_context = stream_context_create($reqPrefs);
-    // $response = file_get_contents($uri, false, $stream_context);
-    // $matches = json_encode($response);
-    // echo $matches;
-    // echo json_encode([$standings, $matches)];
-    // $competition = ['standings' => $standings, 'matches' => $matches];
-    // echo json_encode($dados);
-
-    // switch ($requisicao) {
-    //     case 'classificacao':
-    //         $uri = 'http://api.football-data.org/v4/competitions/CLI/standings';
-    //         $reqPrefs['http']['method'] = 'GET';
-    //         $reqPrefs['http']['header'] = 'X-Auth-Token: bef4843a927541c5b894ff0c1d3c78ae';
-    //         $stream_context = stream_context_create($reqPrefs);
-    //         $response = file_get_contents($uri, false, $stream_context);
-    //         $standings = json_encode($response);
-    //         // $req = $standings;
-    //         echo $standings;
-    //         break;
-    //     case 'partidas':
-    //         $uri = 'http://api.football-data.org/v4/competitions/CLI/standings';
-    //         $reqPrefs['http']['method'] = 'GET';
-    //         $reqPrefs['http']['header'] = 'X-Auth-Token: bef4843a927541c5b894ff0c1d3c78ae';
-    //         $stream_context = stream_context_create($reqPrefs);
-    //         $response = file_get_contents($uri, false, $stream_context);
-    //         $matches = json_encode($response);
-    //         echo $matches;
-    //         break;
-    // }
-    // switch ($requisicao) {
-    //     case 'classificacao':
-    //         echo $standings;
-    //         break;
-    //     case 'partidas':
-    //         echo $matches;
-    //         break;
-    // }
-    exit();
-
 ?>
