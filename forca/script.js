7ddab00e696e22fc63395dc2f24ab32027b44ab1
Filipe Husak <69702727+flp04@users.noreply.github.com>
2024-01-07 19:37:25 -0300

    implementado input invisivel para dispositivos moveis na forca

--- forca/script.js
+++ forca/script.js
@@ -29,6 +29,8 @@ function palavraSecreta() {
     ]
   };
 
+  document.getElementById('inputTeclado').focus()
+
   var dicas = Object.keys(palavras);
   var dicaEscolhida = dicas[Math.floor(Math.random() * dicas.length)];
 
@@ -38,56 +40,47 @@ function palavraSecreta() {
   return { dica: dicaEscolhida, palavra: palavraEscolhida };
 }
 
-// Exemplo de uso
+var data = palavraSecreta();
+document.getElementById('dica').textContent = data.dica
+let palavra = data.palavra
+let palavraFormada = []
+for (let i = 0; i < palavra.length; i++) {
+  let divPalavraSecreta = document.getElementById('palavra-secreta')
+  let divLetra = document.createElement('div')
+  divLetra.setAttribute('class', 'letter-box')
+  divPalavraSecreta.appendChild(divLetra)
+  palavraFormada.push('')
+}
 
-// document.getElementById('inputTeclado').focus()
-// fetch('http://127.0.0.1:5000/palavraSecreta')
-// .then(response => response.json())
-// .then(data => {
-  console.log('entrou')
-  var data = palavraSecreta();
-  console.log('criou a palavra')
-  document.getElementById('dica').textContent = data.dica
-  let palavra = data.palavra
-  let palavraFormada = []
-  for (let i = 0; i < palavra.length; i++) {
-    let divPalavraSecreta = document.getElementById('palavra-secreta')
-    let divLetra = document.createElement('div')
-    divLetra.setAttribute('class', 'letter-box')
-    divPalavraSecreta.appendChild(divLetra)
-    palavraFormada.push('')
-  }
+const container = document.querySelector('#jogo')
+const fireworks = new Fireworks.default(container)
+function startFogos() {
+  fireworks.start()
+}
 
-  const container = document.querySelector('#jogo')
-  const fireworks = new Fireworks.default(container)
-  function startFogos() {
-    fireworks.start()
-  }
-
-  document.addEventListener('keydown', (event) => {
-    tecla = event.key
-    if (/^[a-zA-Z]$/.test(tecla)) {
-      if(palavra.toLowerCase().includes(tecla.toLowerCase())) {
-        for (let i in palavra) {
-          if (palavra[i].toLowerCase() == tecla.toLowerCase()) {
-            document.getElementById('palavra-secreta').children[i].textContent = tecla.toUpperCase()
-            palavraFormada[i] = tecla
-          }
-          if (palavra.toLowerCase() == palavraFormada.join('').toLocaleLowerCase()) {
-            startFogos()
-          }
+document.addEventListener('keydown', (event) => {
+  tecla = event.key
+  if (/^[a-zA-Z]$/.test(tecla)) {
+    if(palavra.toLowerCase().includes(tecla.toLowerCase())) {
+      for (let i in palavra) {
+        if (palavra[i].toLowerCase() == tecla.toLowerCase()) {
+          document.getElementById('palavra-secreta').children[i].textContent = tecla.toUpperCase()
+          palavraFormada[i] = tecla
         }
-      } else {
-        let foi = false
-        partes = ['cabeca','corpo','braco esquerdo','braco direito','perna esquerda','perna direita']
-        boneco = document.getElementById('boneco-palito')
-        for (let i in partes) {
-          if (!foi && document.getElementById(partes[i]).style.display == 'none') {
-            document.getElementById(partes[i]).style.display = 'block'
-            foi = true
-          }
+        if (palavra.toLowerCase() == palavraFormada.join('').toLocaleLowerCase()) {
+          startFogos()
+        }
+      }
+    } else {
+      let foi = false
+      partes = ['cabeca','corpo','braco esquerdo','braco direito','perna esquerda','perna direita']
+      boneco = document.getElementById('boneco-palito')
+      for (let i in partes) {
+        if (!foi && document.getElementById(partes[i]).style.display == 'none') {
+          document.getElementById(partes[i]).style.display = 'block'
+          foi = true
         }
       }
     }
-  })
-// })
+  }
+})
