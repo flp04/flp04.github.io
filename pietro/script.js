387f3d59715889c5fd8ee68c0a9dbd4161a02b68
Filipe Husak <69702727+flp04@users.noreply.github.com>
2024-12-08 13:49:30 -0300

    incluso o programa de soma

--- pietro/script.js
+++ pietro/script.js
@@ -1,4 +1,5 @@
 anoAtual = 2024
 anoNascimento = prompt("insira seu ano de nascimento")
 anoNascimento = parseInt(anoNascimento)
-alert(anoAtual-anoNascimento)
+idade = anoAtual - anoNascimento
+alert("Você tem " + idade + " anos")
---
+++ pietro/soma/script_soma.js
@@ -0,0 +1,6 @@
+z = prompt("Digite um numero por favor")
+z = parseInt(z)
+y = prompt("Digite outro numero")
+y = parseInt(y)
+soma = z+y
+alert("a soma é " + soma)
